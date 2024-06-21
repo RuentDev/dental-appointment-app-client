6aa299f42d4fd236cece54c5f3f4f13ccec127a4
RuentDev <ruent.dev@gmail.com>
2024-06-21 22:55:14 +0800

    update page ui , add stack component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Components from '@/components'
+import { Stack } from '@chakra-ui/react'
 
 const HomePage = () => {
   return (
-    <>
+    <Stack w="100%">
       <Components.LandingSection />
-    </>
+      <Components.Orthodontic />
+    </Stack>
   )
 }
 
