9136c27e05b18f50e1031bb9ce0197e705b8f834
Your Name <leeryan307@gmail.com>
2024-06-22 20:58:31 +0800

    updated to inline css for hover

--- src/components/Cards/OrthodonticCard.tsx
+++ src/components/Cards/OrthodonticCard.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
@@ -13,71 +12,56 @@ interface OrthodonticCardProps {
 }
 
 const OrthodonticCard = (props: OrthodonticCardProps) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const onMouseEnterFunction = () => {
-    setIsHovered(true);
-  };
-
-  const onMouseLeaveFunction = () => {
-    setIsHovered(false);
-  };
-
   return (
     <Container
       position="relative"
       borderWidth={props.borderWidth}
-      onMouseEnter={onMouseEnterFunction}
-      onMouseLeave={onMouseLeaveFunction}
       overflow="hidden"
+      _hover={{ '.labelBox': { display: 'flex' } }}
     >
       <Image
         alt="services"
         width={props.width}
         height={props.height}
         src={props.src}
-        style={{
-          transition: "filter 0.3s ease",
-          filter: isHovered ? "blur(5px)" : "none",
-        }}
       />
-      {isHovered && (
-        <Flex>
-          <Box
-            position="absolute"
-            top="0"
-            left="0"
-            width="100%"
-            height="100%"
-            backgroundColor="rgba(0, 0, 0, 0.50)"
-            transition="0.2s ease"
-          />
-          <Box
-            position="absolute"
-            top="80%"
-            left="50%"
-            transform="translate(-50%, -50%)"
-            width="230px"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            backgroundColor="rgba(24, 175, 211, 0.65)"
-            color="white"
-            textAlign="center"
-            padding="10px"
-          >
-            <Text
-              color="white"
-              fontWeight="bold"
-              fontSize="xl"
-              textAlign="center"
-              p={4}
-            >
-              {props.label}
-            </Text>
-          </Box>
-        </Flex>
-      )}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        backgroundColor="rgba(0, 0, 0, 0)"
+        _hover={{
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
+        }}
+        transition="0.2s ease"
+      />
+      <Box
+        className="labelBox"
+        position="absolute"
+        top="80%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        width="230px"
+        display="none"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="rgba(24, 175, 211, 0.65)"
+        color="white"
+        textAlign="center"
+        padding="10px"
+      >
+        <Text
+          color="white"
+          fontWeight="bold"
+          fontSize="xl"
+          textAlign="center"
+          p={4}
+        >
+          {props.label}
+        </Text>
+      </Box>
     </Container>
   );
 };
